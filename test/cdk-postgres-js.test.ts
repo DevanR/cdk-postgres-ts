b536f5cd70c127cfcc44eb0f640dcc961a4e1b5d
DevanR <devanbing@gmail.com>
2023-07-10 17:28:48 +0800

    Initial commit

---
+++ test/cdk-postgres-js.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPostgresJs from '../lib/cdk-postgres-js-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-postgres-js-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPostgresJs.CdkPostgresJsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
